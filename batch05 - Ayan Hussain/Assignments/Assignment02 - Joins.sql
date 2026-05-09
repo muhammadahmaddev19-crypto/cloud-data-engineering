a6f289e25270c503eb9a04ea0023c8449b01c694
muhammadahmaddev19-crypto <muhammadahmaddev19@gmail.com>
2026-05-09 12:44:51 +0500

    Update Assignment02 - Joins.sql

--- batch05 - Ayan Hussain/Assignments/Assignment02 - Joins.sql
+++ batch05 - Ayan Hussain/Assignments/Assignment02 - Joins.sql
@@ -13,10 +13,13 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
-
+select p.product_name,
+  p.list_price,
+  c.category_name 
+from production.products as p
+inner join production.categories as c
+on p.category_id=c.category_id
+order by p.product_name asc;
 -- ============================================================
 --  Question 2
 --  Show the customer full name (as full_name), order_id,
@@ -26,9 +29,13 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT (c.first_name +' ' + c.last_name) AS full_name,  
+  o.order_id, 
+  o.order_date 
+FROM sales.customers as c
+INNER JOIN sales.orders as o 
+ON c.customer_id = o.customer_id
+ORDER BY o.order_date DESC;
 
 -- ============================================================
 --  Question 3
@@ -40,9 +47,17 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT  
+  p.product_name,  
+  p.list_price,  
+  c.category_name,  
+  b.brand_name 
+FROM production.products p
+JOIN production.categories c
+ON p.category_id = c.category_id
+JOIN production.brands b 
+ON p.brand_id = b.brand_id 
+ORDER BY b.brand_name, p.product_name;
 
 -- ============================================================
 --  Question 4
@@ -55,9 +70,14 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT 
+    p.product_name,
+    oi.order_id,
+    oi.item_id
+FROM production.products p
+LEFT JOIN sales.order_items oi 
+ON p.product_id = oi.product_id
+ORDER BY oi.order_id ASC;
 
 -- ============================================================
 --  Question 5
@@ -68,9 +88,13 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT 
+    p.product_id, 
+    p.product_name
+FROM production.products p
+LEFT JOIN sales.order_items oi 
+ON p.product_id = oi.product_id
+WHERE oi.order_id IS NULL;
 
 -- ============================================================
 --  Question 6
@@ -83,9 +107,14 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT 
+    s.store_name,
+    s.store_id,
+    o.order_id,
+    o.order_date
+FROM sales.stores s
+LEFT JOIN sales.orders o 
+ON s.store_id = o.store_id;
 
 -- ============================================================
 --  Question 7
@@ -98,9 +127,12 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT 
+    (s.first_name + ' ' + s.last_name) AS staff_name,
+    (m.first_name+ ' '+ m.last_name) AS manager_name
+FROM sales.staffs s
+INNER JOIN sales.staffs m 
+ON s.manager_id = m.staff_id;
 
 -- ============================================================
 --  Question 8
@@ -113,9 +145,11 @@
 -- ============================================================
 
 -- Write your query below:
-
-
-
+SELECT 
+    s.store_name, 
+    b.brand_name
+FROM sales.stores s
+CROSS JOIN production.brands b;
 
 -- ============================================================
 --  Question 9
@@ -128,3 +162,17 @@
 -- ============================================================
 
 -- Write your query below:
+SELECT 
+    (c.first_name+ ' '+ c.last_name) AS full_name,
+    o.order_id,
+    o.order_date,
+    p.product_name,
+    oi.list_price
+FROM sales.customers c
+JOIN sales.orders o 
+ON c.customer_id = o.customer_id
+JOIN sales.order_items oi 
+ON o.order_id = oi.order_id
+JOIN production.products p 
+ON oi.product_id = p.product_id
+ORDER BY o.order_date ASC, full_name ASC;
